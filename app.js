ecfa290440eae9d90a36d84dba17a81b5ff33fb5
Rauan <raauans@gmail.com>
2025-08-06 15:28:20 -0300

    new version

---
+++ app.js
@@ -0,0 +1,93 @@
+let amigos = [];
+let sorteioMap = {};
+
+function adicionarAmigo() {
+    let campoNome = document.querySelector('input');
+    if (campoNome.value === '') {
+        alert("Por favor, digite um nome!");
+        return;
+    }
+    amigos.push(campoNome.value);
+    exibirNomesAmigos();
+    campoNome.value = '';
+}
+
+function exibirNomesAmigos() {
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+
+    amigos.forEach((nome, i) => {
+        const li = document.createElement('li');
+        li.innerHTML = nome;
+
+        const btnExcluir = document.createElement('button');
+        btnExcluir.innerText = 'X';
+        btnExcluir.classList.add('botao-excluir');
+        btnExcluir.onclick = () => {
+            amigos.splice(i, 1);
+            exibirNomesAmigos();
+        };
+
+        const btnEditar = document.createElement('button');
+        btnEditar.innerText = '✎';
+        btnEditar.classList.add('botao-editar');
+        btnEditar.onclick = () => {
+            const novoNome = prompt("Digite o novo nome:");
+            if (novoNome) amigos[i] = novoNome;
+            exibirNomesAmigos();
+        };
+
+        li.appendChild(btnExcluir);
+        li.appendChild(btnEditar);
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigos() {
+    if (amigos.length < 2) {
+        alert("Adicione pelo menos dois nomes para sortear.");
+        return;
+    }
+
+    let valido = false;
+    let embaralhado = [];
+
+    while (!valido) {
+        embaralhado = [...amigos].sort(() => Math.random() - 0.5);
+        valido = amigos.every((nome, i) => nome !== embaralhado[i]);
+    }
+
+    sorteioMap = {};
+    amigos.forEach((nome, i) => {
+        sorteioMap[nome] = embaralhado[i];
+    });
+
+    popularSelect();
+    document.getElementById('resultado').innerHTML = '';
+}
+
+function popularSelect() {
+    const select = document.getElementById('selecaoPessoa');
+    select.innerHTML = '<option value="">Selecione seu nome para ver o sorteio</option>';
+    amigos.forEach(nome => {
+        const option = document.createElement('option');
+        option.value = nome;
+        option.textContent = nome;
+        select.appendChild(option);
+    });
+}
+
+function mostrarResultado() {
+    const select = document.getElementById('selecaoPessoa');
+    const nomeSelecionado = select.value;
+    const resultado = document.getElementById('resultado');
+
+    if (nomeSelecionado && sorteioMap[nomeSelecionado]) {
+        resultado.innerHTML = `<p>${nomeSelecionado}, você tirou <strong>${sorteioMap[nomeSelecionado]}</strong>!</p>`;
+
+        setTimeout(() => {
+            resultado.innerHTML = '';
+            select.value = '';
+        }, 10000);
+    }
+}
